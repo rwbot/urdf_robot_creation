dd1fffefee8c065c3ecb9d07e24abb8fd3cac69e
rwbot <user@ip-172-31-43-124.eu-west-1.compute.internal>
2018-06-23 03:24:43 +0000

    finished rviz spawn, and added collisions and inertials. currently spawning in gazebo. spawning only the 3 ball feet seems normal, but spawning everything together, both upper and lower leg links fall under ground plane when gurdy spawns, and drops a little. next move might be to measure each joint and spawn each so that its perfectly in a standing pose

---
+++ my_gurdy_description/src/remove_gurdy.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+import rospy
+from gazebo_msgs.srv import DeleteModel, DeleteModelRequest # Import the service message used by the service /gazebo/delete_model
+import sys 
+
+rospy.init_node('remove_model_service_client') # Initialise a ROS node with the name service_client
+print "Waiting for Service /gazebo/delete_model"
+rospy.wait_for_service('/gazebo/delete_model') # Wait for the service client /gazebo/delete_model to be running
+delete_model_service = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel) # Create the connection to the service
+kk = DeleteModelRequest() # Create an object of type DeleteModelRequest
+kk.model_name = "gurdy" # Fill the variable model_name of this object with the desired value
+print "Deleting model ="+str(kk)
+status_message = delete_model_service(kk) # Send through the connection the name of the object to be deleted by the service
+print status_message
